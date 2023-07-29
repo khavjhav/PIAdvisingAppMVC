2cb1fc002b5af715b39cb8fd0ea49c641e089801
Arnab Barua <ournob@outlook.com>
2023-07-29 13:14:39 +0600

    js save issue

--- PIAdvisingApp/ViewModels/SaveApiFromBondRequest.cs
+++ PIAdvisingApp/ViewModels/SaveApiFromBondRequest.cs
@@ -27,18 +27,19 @@ namespace PIAdvisingApp.ViewModels
         public string MeasureUnit { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal BookingQty { get; set; }
+        public decimal QtyInKG { get; set; }
         public string QuantityUnit { get; set; }
         public decimal Val2 { get; set; }
         public string BondRemarks { get; set; }
 
         // Nullable properties
-        public string? PONumber { get; set; }
-        public string? StyleRef { get; set; }
-        public string? Color { get; set; }
-        public string? BreakDown1 { get; set; }
-        public string? BreakDown2 { get; set; }
-        public string? ShadeNumber { get; set; }
-        public string? WashType { get; set; }
-        public string? GSM { get; set; }
+        public string PONumber { get; set; }
+        public string StyleRef { get; set; }
+        public string Color { get; set; }
+        public string  BreakDown1 { get; set; }
+        public string  BreakDown2 { get; set; }
+        public string  ShadeNumber { get; set; }
+        public string WashType { get; set; }
+        public string GSM { get; set; }
     }
 }
